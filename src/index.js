7444f8ae92ab6a6e4c263d56626ce6457c66a468
Githmin Jayawardhana <githmin21@gmail.com>
2022-08-19 01:31:07 +0530

    added index js function where it calls external js files

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ import openAbout from '../src/about.js'
 // openHome();
 // openMenu();
 // openAbout();
+
+let home = document.getElementById('home');
+home.addEventListener("click", openHome);
+
+let menu = document.getElementById('menu');
+menu.addEventListener("click", openMenu);
+
+let about = document.getElementById('about');
+about.addEventListener("click", openAbout);
